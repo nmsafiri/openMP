4375a5c03bcc054d604bb7fee0f5df64e1d6360b
nmsafiri <nmbwamb@g.clemson.edu>
2019-08-24 20:30:59 -0400

    parallel prog trial 1

---
+++ pprog.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<omp.h>
+
+int main(){
+	const int N = 10000;
+	int i; 
+	int a[N], b[N], c[N]; 
+	for (i = 0; i<N; i++){
+		a[i]=i*2;
+		b[i]=i;
+	}
+#pragma omp parallel shared(a,b,c) private(i)
+	{
+#pragma omp for
+		for(i=0;i<N;i++){
+			c[i]=a[i] + b[i];
+
+		}
+	}
+
+	printf("%d ", c[10]);
+}
+
